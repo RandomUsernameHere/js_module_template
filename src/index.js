fc9abc8f459956606abbc947349aca8b4ad85066
Mitin Aleksandr <jamanphonehtc@gmail.com>
2017-01-15 19:09:52 +0200

    Version added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-
 /*
  * Стандартный шаблон для модуля,
  * предполагающего многократное использование
+ * version 1.0.0
+ * repository https://github.com/RandomUsernameHere/js_module_template
  */
-;(function( window, $){
+;(function (window, $) {
 
     /* Метаданные модуля */
     var Metadata = {
@@ -13,40 +14,39 @@
             version: '',//Package version
             license: '',//Package description
             repository: ''//Repository url
-        }
-        ;
+    };
 
     /*Флаг остановки по отсутсвию зависимости*/
     var stopRequired = false;
 
     /*Проверки на отсутсвие зависимостей*/
-    (function(){
-        if( window === undefined){
+    (function () {
+        if (window === undefined) {
             console.error('window is required');
             stopRequired = true;
             return false;
         }
 
-        if( $ === undefined){
+        if ($ === undefined) {
             console.error('jQuery is required');
             stopRequired = true;
             return false;
         }
     })();
 
-    if(stopRequired){
+    if (stopRequired) {
         return false;
     }
 
     /*Устанавливаем неинициализированный объект в глобальное пространство имен*/
-    window.testModule = new function(){
+    window.testModule = new function () {
         var that = this;
 
         /*Опции и настройки*/
         this.options = {};
 
         /*Инициализация*/
-        this.init = function( options){
+        this.init = function (options) {
             this.options = $.extend(this.options, this.getDefaults, options);
 
             //Если все в порядке, возвращаем объект
@@ -54,17 +54,18 @@
         };
 
         /*Метод, который возвращает объект с умолчаниями*/
-        this.getDefaults = function(){
+        this.getDefaults = function () {
             return {};
         };
 
         /*Возвращает метаданные модуля*/
-        this.getMetadata = function(){
+        this.getMetadata = function () {
             return Metadata;
         };
 
         /*В этом методе, по возможности, следует установить обработчики событий*/
-        this.addEventHandlers = function(){ };
+        this.addEventHandlers = function () {
+        };
     };
 
 }(window, jQuery));
